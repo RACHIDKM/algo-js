d3be24a59b660ab10167d23f3d7b31518ddeccd0
RACHIDKM <rkarym7@gmail.com>
2023-11-09 15:24:48 +0100

    fisrt commit

---
+++ index.js
@@ -0,0 +1,110 @@
+// for(let i = 1 ; i <= 100 ; i++){
+//     if(i % 3 === 0 & i % 5 === 0 ){
+//         console.log("fizzBuzz")
+//     }else if( i % 3 === 0 ){
+//           console.log("fizz")
+//     }else if( i % 5 === 0 ){
+//     console.log("Buzz")
+// }else{
+//     console.log(i)
+// }
+// // }
+
+
+
+
+
+let perfectParentieses  = "(((((((((((((())()))(()()()))))))))))))("
+
+ for(let i = 0 ;i < perfectParentieses.length  ; i++ ){
+ console.log( perfectParentieses[i])
+
+ }
+
+ let open = 0
+ let close = 0
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  let perfectParentieses = "(((((((((((((()))()))()))(()()()))))))))))))("
+
+// for(let i = 0 ;i < perfectParentieses.length  ; i++ ){
+//    if(perfectParentieses[i] === '('){
+//     open++
+//    }else if(perfectParentieses[i] === ')'){
+//     close= close + 1
+    
+//  }
+    
+// }
+
+
+
+
+
+
+// console.log('open ===>>>',open)
+// console.log('close ==>>>',close)
+
+
+// circuitPower(230, 10) ➞ 2300
+
+// circuitPower(110, 3) ➞ 330
+
+// circuitPower(480, 20) ➞ 9600
+
+
+
+
+// function circuitPower(voltage, current) {
+   
+//     return voltage * current
+// }
+// console
+
+
+
+
+// function check(a, x) {
+//     return a.includes(x);
+// //   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
